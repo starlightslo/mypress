e83ffbd6701b43471313b5eff40441608a95dcb0
Tony Huang <tony@huang.ninja>
2016-03-17 18:28:05 +0800

    Adding database error handling.

--- app/middlewares/database.js
+++ app/middlewares/database.js
@@ -6,12 +6,19 @@ const config = require('../../config/config')
 // Loading database
 const db = require('../../config/database')(config.db)
 
+// Define
+const ERROR_DATABASE_NOT_EXISTING = 'The database does not existing.'
 
 /*
  * This is the middleware of database, which will set the database instance to the express app.
  */
 module.exports = function(req, res, next) {
-	// Set db into express app
-	req.app.set('db', db)
-	next()
+	// Check the database instance
+	if (!db.normalDB || !db.adminDB) {
+		next(ERROR_DATABASE_NOT_EXISTING)
+	} else {
+		// Set db into express app
+		req.app.set('db', db)
+		next()
+	}
 }
