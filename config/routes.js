b8cec21ddf0198a81d16bd2fdd8ca6dfcd561889
Tony Huang <tony@huang.ninja>
2016-03-19 14:55:56 +0800

    Adding a sample template and supporting multiple templates feature.

--- config/routes.js
+++ config/routes.js
@@ -2,10 +2,26 @@
 
 const indexRouter = require('../app/routes/index')
 
+const changeStaticPathForTemplate = (req, res, next) => {
+	// Getting the template
+	const template = req.app.get('template')
+
+	// Modifing the request url
+	req.url = '/' + template + req.url
+
+	next()
+}
 
 /**
  * Expose routes
  */
 module.exports = function (app) {
 	app.use('/', indexRouter)
+
+
+	/**
+	 * Change the request path for template
+	 * IMPORTANT: This should be after all process of routes!!!
+	 */
+	app.use(changeStaticPathForTemplate)
 }
