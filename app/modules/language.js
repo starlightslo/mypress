0daf30630d005cffbd0aec8d6b4ed394dde5feec
Tony Huang <tony@huang.ninja>
2016-03-20 21:49:11 +0800

    Removed log code.

--- app/modules/language.js
+++ app/modules/language.js
@@ -10,17 +10,12 @@ class Language {
 	}
 
 	static getTemplateLanguage(template, language) {
-		console.log(template)
-		console.log(language)
 		let filePath = path.join(config.root, 'app/languages', template + '_' + language + '.js')
-		console.log(filePath)
 		if (fs.existsSync(filePath)) {
-			console.log('exist')
 			return require('../languages/' + template + '_' + language)
 		} else {
 			filePath = path.join(config.root, 'app/languages', template + '.js')
 			if (fs.existsSync(filePath)) {
-				console.log('default exist')
 				return require('../languages/' + template)
 			}
 		}
