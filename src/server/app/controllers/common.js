2916a615f244943a005702ea95cbadd849594fce
Tony Huang <tony@huang.ninja>
2017-04-29 21:53:31 +0800

    Added loading settings data from database and handling the data with language.

---
+++ src/server/app/controllers/common.js
@@ -0,0 +1,28 @@
+const _ = require('lodash/core');
+const LanugageDataModel = require('../data-models/lanugage');
+
+const DEFAULT = 'default';
+const SYSTEM = 'system';
+
+exports.settings = (request, reply) => {
+    const lanugageDataModel = new LanugageDataModel(request);
+    let text = {};
+    let settings = {};
+
+    // Loading system and template text
+    text = _.extend(text, request.server.text[SYSTEM][lanugageDataModel.getLanguage()]);
+    text = _.extend(text, request.server.text[request.server.template][lanugageDataModel.getLanguage()]);
+
+    // Loading settings
+    settings = request.server.settings[lanugageDataModel.getLanguage()];
+    if (!settings) {
+        settings = request.server.settings[DEFAULT];
+    }
+
+    const data = {
+        language: lanugageDataModel.getLanguage(),
+        text: text,
+        settings: settings
+    };
+    return reply(data);
+};
